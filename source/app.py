3ccea293fc336d2434581af0876b1f16ece7ef7b
hlemarchant <hugo.lemarchant@insa-cvl.fr>
2021-06-15 16:29:48 +0900

    update Tuesday afternoon, app working on linux

--- source/app.py
+++ source/app.py
@@ -663,7 +663,6 @@ class MainWindow(QtWidgets.QMainWindow):
         for key, end_index in zip(self.landmarks_categories, self.landmarks_indexes):
             if key in labels.keys():
                 to_copy = labels[key]
-                print(to_copy)
                 visibility = self.visibility[start_index:end_index]
                 index_to_copy = 0
                 for id in visibility:
@@ -673,7 +672,8 @@ class MainWindow(QtWidgets.QMainWindow):
                     else:
                         new_landmarks.append([0, 0])
             else:
-                new_landmarks.append([0*(end_index - start_index + 1)])
+                for _ in range(end_index - start_index):
+                    new_landmarks.append([0])
             start_index = end_index
         with open(os.path.join("annotations", self.art_id, self.img_id + "_2D.json"), 'w+') as f:
             data = {"art_id": self.art_id, "img_id": self.img_id, "landmarks": new_landmarks,
@@ -701,7 +701,6 @@ class MainWindow(QtWidgets.QMainWindow):
                     shapes.append({'label': category, 'points': tmp, 'shape_type': 'linestrip', 'flags': {},
                                    'group_id': None, 'other_data': {}})
                 start_index = end_index
-        print(shapes)
         return shapes
 
     def menu(self, title, actions=None):
